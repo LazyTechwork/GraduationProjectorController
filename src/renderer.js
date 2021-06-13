225a9f2b524eb146785dbf6424c8cd0da104a83e
LazyTechwork <lazytechwork@gmail.com>
2021-06-13 22:04:23 +0300

    Working on projector open

--- src/renderer.js
+++ src/renderer.js
@@ -1,31 +1,18 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './index.css';
+import fs from "fs";
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const {ipcRenderer} = require("electron");
+const openButton = document.getElementById("openProjectorWindow");
+openButton.addEventListener("click", () => {
+    ipcRenderer.send("projector-window");
+});
+document.getElementById("runProjector").addEventListener("click", () => {
+    ipcRenderer.send("run-projector");
+});
+ipcRenderer.on("projector-window-state", (event, args) => {
+    if (args === 'closed')
+        openButton.innerHTML = "Открыть проектор";
+    else if (args === 'opened')
+        openButton.innerHTML = "Закрыть проектор";
+})
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,13 +1,28 @@
 const rules = require('./webpack.rules');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const assets = ["static"];
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+    test: /\.css$/,
+    use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader'}
+    ]
 });
 
 module.exports = {
-  // Put your normal webpack config below here
-  module: {
-    rules,
-  },
+    // Put your normal webpack config below here
+    module: {
+        rules,
+    },
+    plugins: assets.map(asset => {
+        return new CopyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, 'src', asset),
+                to: path.resolve(__dirname, '.webpack/renderer', asset)
+            }]
+        });
+    })
+
 };
